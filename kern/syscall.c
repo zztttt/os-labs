6d98a23def80cf989fd14cadf856d427f9fc0ff3
zztttt <1315691850@qq.com>
2019-05-10 22:22:05 +0800

    finish part C

--- kern/syscall.c
+++ kern/syscall.c
@@ -313,7 +313,34 @@ static int
 sys_ipc_try_send(envid_t envid, uint32_t value, void *srcva, unsigned perm)
 {
 	// LAB 4: Your code here.
-	panic("sys_ipc_try_send not implemented");
+	//panic("sys_ipc_try_send not implemented");
+	struct Env *e;
+	int r;
+	if((r = envid2env(envid, &e, 0) ) < 0)
+		return r;
+	if(!e->env_ipc_recving)
+		return -E_IPC_NOT_RECV;
+	if(srcva < (void*)UTOP){
+		if(PGOFF(srcva) || (perm & (PTE_U | PTE_P)) != (PTE_U | PTE_P) || (perm & (~PTE_SYSCALL)))
+		return -E_INVAL;
+		pte_t *pte;
+		struct PageInfo *pg;
+		if(!(pg = page_lookup(curenv->env_pgdir, srcva, &pte)))
+		return -E_INVAL;
+		if((*pte & perm) != perm)
+		return -E_INVAL;
+		if(e->env_ipc_dstva < (void *)UTOP){
+		if((r = page_insert(e->env_pgdir, pg, e->env_ipc_dstva, perm)) < 0)
+			return r;
+		}
+	}
+	e->env_ipc_recving        = 0;
+	e->env_ipc_from           = curenv->env_id;
+	e->env_ipc_value          = value;
+	e->env_ipc_perm           = perm;
+	e->env_status             = ENV_RUNNABLE;
+	e->env_tf.tf_regs.reg_eax = 0;
+	return 0;
 }
 
 // Block until a value is ready.  Record that you want to receive
@@ -331,8 +358,15 @@ static int
 sys_ipc_recv(void *dstva)
 {
 	// LAB 4: Your code here.
-	panic("sys_ipc_recv not implemented");
-	return 0;
+	//panic("sys_ipc_recv not implemented");
+	if(!(dstva < (void*)UTOP) || !PGOFF(dstva)){
+		curenv->env_ipc_recving = 1;
+		curenv->env_ipc_dstva   = dstva;
+		curenv->env_status      = ENV_NOT_RUNNABLE;
+		sched_yield();
+		return 0;
+	}
+  return -E_INVAL;
 }
 
 static int
@@ -397,11 +431,9 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 			sys_yield();
 			return 0;
 		case SYS_ipc_try_send:
-			cprintf("syscallno not implement = %d\n",syscallno);
-			return -E_INVAL;
+  			return sys_ipc_try_send((envid_t)a1, (uint32_t)a2, (void *)a3, (unsigned)a4);
 		case SYS_ipc_recv:
-			cprintf("syscallno not implement = %d\n",syscallno);
-			return -E_INVAL;
+  			return sys_ipc_recv((void *)a1);
 		case SYS_map_kernel_page:
 		  	return sys_map_kernel_page((void *)a1, (void *)a2);
 		case SYS_sbrk:
