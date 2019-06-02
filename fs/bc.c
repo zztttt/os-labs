dfd580e36bf26af013ae2157bfa79170e174b6e6
zztttt <1315691850@qq.com>
2019-06-02 15:15:07 +0800

    finish lab5

--- fs/bc.c
+++ fs/bc.c
@@ -1,17 +1,21 @@
 
 #include "fs.h"
+
 #ifdef USE_EVICT_POLICY
 static void* bc_list[NBLOCKCACHE];
 static uint32_t next_free_bc = 0;
 static uint32_t clock_arm = 0;
-
 // Use clock page-removal algorithm
-static void clock_bc_pgfault(void *addr, uint32_t blockno) {
+static void clock_remove(void *addr, uint32_t blockno) {
 	int r;
 	void* candidate_addr;
 
-	static_assert(NBLOCKCACHE > 0);
-	assert((uint32_t)addr % PGSIZE == 0);
+	if(NBLOCKCACHE <= 0){
+		panic("NBLOCKCACHE error");
+	}
+	if((uint32_t)addr % PGSIZE != 0){
+		panic("addr error");
+	}
 	if (next_free_bc < NBLOCKCACHE) {
 		cprintf("No eviction!! Map block %u to 0x%08x\n", blockno, (uint32_t)addr);
 		if ((r = sys_page_alloc(0, addr, PTE_P | PTE_U | PTE_W)) < 0)
@@ -24,45 +28,36 @@ static void clock_bc_pgfault(void *addr, uint32_t blockno) {
 
 		bc_list[next_free_bc++] = addr;
 	} else {
-		// 1. Find the eviction candidate
-		while(1) {
-			candidate_addr = bc_list[clock_arm];
-			if (uvpt[PGNUM(candidate_addr)] & PTE_A) {
-				// 2. Flush the candidate because both the dirty bit and access bit are cleared
-				if (uvpt[PGNUM(candidate_addr)] & PTE_D)
+		// Find the eviction candidate
+		candidate_addr = bc_list[clock_arm];
+		while(uvpt[PGNUM(candidate_addr)] & PTE_A){
+			//Flush the candidate because both the dirty bit and access bit are cleared
+			if (uvpt[PGNUM(candidate_addr)] & PTE_D)
 					flush_block(candidate_addr);
-				// 3. Clear access bit and dirty bit
-				if ((r = sys_page_map(0, candidate_addr, 0, candidate_addr, PTE_SYSCALL)) < 0)
-					panic("sys_page_map: %e", r);
-				clock_arm = (clock_arm + 1) % NBLOCKCACHE;
-			} else
-				break;
+			//Clear access bit and dirty bit
+			if ((r = sys_page_map(0, candidate_addr, 0, candidate_addr, PTE_SYSCALL)) < 0)
+				panic("sys_page_map: %e", r);
+			clock_arm = (clock_arm + 1) % NBLOCKCACHE;
+			candidate_addr = bc_list[clock_arm];
 		}
-
 		cprintf("Eviction!! Evict block %u. Clock arm %u\n", blockno, clock_arm);
 		cprintf("Evict 0x%08x with 0x%08x\n",(uint32_t)candidate_addr, (uint32_t)addr);
-
-		// 2. Flush the candidate
+		// lush the candidate
 		if (uvpt[PGNUM(candidate_addr)] & PTE_D)
 			flush_block(candidate_addr);
-
-		// 3. Map new page
+		// Map new page
 		if ((r = sys_page_map(0, candidate_addr, 0, addr, PTE_SYSCALL)) < 0)
 			panic("sys_page_map: %e", r);
-
-		// 4. Unmap the old page
+		// Unmap the old page
 		if ((r = sys_page_unmap(0, candidate_addr)) < 0)
 			panic("sys_page_unmap: %e", r);
-
-		// 5. Read data from disk to memory
+		// Read data from disk to memory
 		if ((r = ide_read(blockno * BLKSECTS, addr, BLKSECTS)) < 0)
 			panic("ide_read: %e", r);
-
-		// 6. Clear dirty bit
+		// Clear dirty bit
 		if ((r = sys_page_map(0, addr, 0, addr, PTE_SYSCALL)) < 0)
 			panic("sys_page_map: %e", r);
-
-		// 7. Update bc list
+		// Update bc list
 		bc_list[clock_arm] = addr;
 	}
 }
@@ -109,17 +104,13 @@ bc_pgfault(struct UTrapframe *utf)
 	if (super && blockno >= super->s_nblocks)
 		panic("reading non-existent block %08x\n", blockno);
 
-	// Allocate a page in the disk map region, read the contents
-	// of the block from the disk into that page.
-	// Hint: first round addr to page boundary. fs/ide.c has code to read
-	// the disk.
-	//
-	// LAB 5: you code here:
 #ifndef USE_EVICT_POLICY
 	// Allocate a page in the disk map region, read the contents
 	// of the block from the disk into that page, and mark the
 	// page not-dirty (since reading the data from disk will mark
 	// the page dirty).
+	// Hint: first round addr to page boundary.s/ide.c has code to read
+	// the disk.
 	//
 	// LAB 5: Your code here
 	if ((r = sys_page_alloc(0, ROUNDDOWN(addr, PGSIZE), PTE_P | PTE_U | PTE_W)) < 0)
@@ -130,7 +121,6 @@ bc_pgfault(struct UTrapframe *utf)
 	if ((r = sys_page_map(0, ROUNDDOWN(addr, PGSIZE), 0, ROUNDDOWN(addr, PGSIZE), PTE_SYSCALL)) < 0)
 		panic("sys_page_map: %e", r);
 #else
-	// Dispatch pgfault
 	if (blockno <= 2) {
 		if ((r = sys_page_alloc(0, ROUNDDOWN(addr, PGSIZE), PTE_P | PTE_U | PTE_W)) < 0)
 			panic("sys_page_alloc: %e", r);
@@ -140,7 +130,7 @@ bc_pgfault(struct UTrapframe *utf)
 		if ((r = sys_page_map(0, ROUNDDOWN(addr, PGSIZE), 0, ROUNDDOWN(addr, PGSIZE), PTE_SYSCALL)) < 0)
 			panic("sys_page_map: %e", r);
 	} else
-		clock_bc_pgfault(ROUNDDOWN(addr, PGSIZE), blockno);
+		clock_remove(ROUNDDOWN(addr, PGSIZE), blockno);
 #endif
 
 	// Check that the block we read was allocated. (exercise for
@@ -148,20 +138,6 @@ bc_pgfault(struct UTrapframe *utf)
 	// in?)
 	if (bitmap && block_is_free(blockno))
 		panic("reading free block %08x\n", blockno);
-	/*if((r = sys_page_alloc((envid_t)0, ROUNDDOWN(addr, PGSIZE), PTE_P | PTE_U | PTE_W)) < 0 )
-		panic("sys_page_alloc: %e",r);
-	ide_read(blockno * BLKSECTS, ROUNDDOWN(addr, PGSIZE), BLKSECTS);
-
-	// Clear the dirty bit for the disk block page since we just read the
-	// block from disk
-	if ((r = sys_page_map(0, addr, 0, addr, uvpt[PGNUM(addr)] & PTE_SYSCALL)) < 0)
-		panic("in bc_pgfault, sys_page_map: %e", r);
-
-	// Check that the block we read was allocated. (exercise for
-	// the reader: why do we do this *after* reading the block
-	// in?)
-	if (bitmap && block_is_free(blockno))
-		panic("reading free block %08x\n", blockno);*/
 }
 
 // Flush the contents of the block containing VA out to disk if
